bb5b92cbe6f67c70619594d0a1df0dd4759e8a00
singhshivam <singhshivam.172@gmail.com>
2020-05-27 15:35:45 -0500

    adding vr proj

---
+++ content/featured/VRSpaceExploration/index.md
@@ -0,0 +1,13 @@
+---
+date: '3'
+title: 'VR Space Exploration'
+cover: './spaceExploration.png'
+external: 'https://codefiddle.wordpress.com/2020/05/25/i-developed-my-first-vr-game-in-less-than-10-hours/'
+tech:
+  - Unity3d
+  - C#
+  - Oculus
+showInProjects: true
+---
+
+The game starts with our main character inside a spaceship. He has to receive a floating cargo from space and navigate to a special planet. On the planet surface, there is a greenhouse. Here the user would be given two tasks which he has to perform.
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
   IconGitHub,
@@ -53,69 +53,46 @@ const StyledGitHubLink = styled.a`
   padding: 10px;
 `;
 
-const Footer = () => {
-  const [setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
-  return (
-    <StyledContainer>
-      <StyledSocial>
-        <StyledSocialList>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <StyledSocialLink
-                  href={url}
-                  target="_blank"
-                  rel="nofollow noopener noreferrer"
-                  aria-label={name}>
-                  {name === 'GitHub' ? (
-                    <IconGitHub />
-                  ) : name === 'Linkedin' ? (
-                    <IconLinkedin />
-                  ) : name === 'Codepen' ? (
-                    <IconCodepen />
-                  ) : name === 'Instagram' ? (
-                    <IconInstagram />
-                  ) : name === 'Twitter' ? (
-                    <IconTwitter />
-                  ) : (
-                    <IconGitHub />
-                  )}
-                </StyledSocialLink>
-              </li>
-            ))}
-        </StyledSocialList>
-      </StyledSocial>
-      <StyledMetadata tabindex="-1">
-        <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
-          target="_blank"
-          rel="nofollow noopener noreferrer">
-          <div>Thanks Brittany Chiang for design</div>
-        </StyledGitHubLink>
-      </StyledMetadata>
-    </StyledContainer>
-  );
-};
+const Footer = () => (
+  <StyledContainer>
+    <StyledSocial>
+      <StyledSocialList>
+        {socialMedia &&
+          socialMedia.map(({ name, url }, i) => (
+            <li key={i}>
+              <StyledSocialLink
+                href={url}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                aria-label={name}>
+                {name === 'GitHub' ? (
+                  <IconGitHub />
+                ) : name === 'Linkedin' ? (
+                  <IconLinkedin />
+                ) : name === 'Codepen' ? (
+                  <IconCodepen />
+                ) : name === 'Instagram' ? (
+                  <IconInstagram />
+                ) : name === 'Twitter' ? (
+                  <IconTwitter />
+                ) : (
+                  <IconGitHub />
+                )}
+              </StyledSocialLink>
+            </li>
+          ))}
+      </StyledSocialList>
+    </StyledSocial>
+    <StyledMetadata tabindex="-1">
+      <StyledGitHubLink
+        href="https://github.com/bchiang7/v4"
+        target="_blank"
+        rel="nofollow noopener noreferrer">
+        <div>Thanks Brittany Chiang for design</div>
+      </StyledGitHubLink>
+    </StyledMetadata>
+  </StyledContainer>
+);
 
 Footer.propTypes = {
   githubInfo: PropTypes.object,
